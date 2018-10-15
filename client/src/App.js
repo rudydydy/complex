83bc5ef1f0ff48394506db115e6045098b6dcb82
Rudy Pangestu <phang.rudy94@gmail.com>
2018-10-15 21:36:39 +0700

    add dockerfile for each module

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import '/App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
